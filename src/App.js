d1ff655927d6199be5b16f3f4d9df704e42c79de
Tdmaxer <ytdilshan07@gmail.com>
2025-04-06 14:45:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/CommonStyles.css';
+import WelcomeScreen from './screens/WelcomeScreen';
+import VehicleTypeScreen from './screens/VehicleTypeScreen';
+import FuelTypeScreen from './screens/FuelTypeScreen';
+import MapSelectionScreen from './screens/MapSelectionScreen';
+import ResultsScreen from './screens/ResultsScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<WelcomeScreen />} />
+          <Route path="/vehicle-type" element={<VehicleTypeScreen />} />
+          <Route path="/fuel-type" element={<FuelTypeScreen />} />
+          <Route path="/map-selection" element={<MapSelectionScreen />} />
+          <Route path="/results" element={<ResultsScreen />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
